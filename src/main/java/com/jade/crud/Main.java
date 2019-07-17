14b2404e2f3e971174f8dcca9f678ee30b5a2649
ELENO <goeleno@gmail.com>
2019-07-17 13:57:45 -0500

    se quitan componentes inesesarios de prueba de email

--- src/main/java/com/jade/crud/Main.java
+++ src/main/java/com/jade/crud/Main.java
@@ -5,8 +5,6 @@
  */
 package com.jade.crud;
 
-import com.jade.crud.entity.Mail;
-import com.jade.crud.service.MailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -26,16 +24,6 @@ import org.springframework.context.ApplicationContext;
 public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-//            Mail mail = new Mail();
-//            mail.setMailFrom("goeleno@gmail.com");
-//            mail.setMailTo("goaguavida@gmail.com");
-//            mail.setMailSubject("Spring Boot - Email Example");
-//            mail.setMailContent("Learn How to send Email using Spring Boot!!!\n\nThanks\nwww.technicalkeeda.com");
-//
-//            ApplicationContext ctx = SpringApplication.run(Main.class, args);
-//            MailService mailService = (MailService) ctx.getBean("mailService");
-//            mailService.sendEmail(mail);
-        
             SpringApplication.run(Main.class, args);
 	}
 	@Override
