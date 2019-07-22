ecf03e817966e7cf3cd90ad81d9ebd822ed61c6d
ELENO <goeleno@gmail.com>
2019-07-22 09:16:32 -0500

    se da formato a el archivo

--- src/main/java/com/jade/crud/controller/UploadFileController.java
+++ src/main/java/com/jade/crud/controller/UploadFileController.java
@@ -9,7 +9,7 @@ import com.jade.crud.servces.StorageService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -60,49 +59,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
- 
 @Controller
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping
 public class UploadFileController {
- 
-  @Autowired
-  StorageService storageService;
- 
-  List<String> files = new ArrayList<String>();
- 
-  @PostMapping("/postFileUpload")
-  public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
-    String message = "";
-    try {
-      storageService.store(file);
-      files.add(file.getOriginalFilename());
- 
-      message = "You successfully uploaded " + file.getOriginalFilename() + "!";
-      return ResponseEntity.status(HttpStatus.OK).body(message);
-    } catch (Exception e) {
-      message = "FAIL to upload " + file.getOriginalFilename() + "!";
-      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+
+    @Autowired
+    StorageService storageService;
+
+    List<String> files = new ArrayList<String>();
+
+    @PostMapping("/postFileUpload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.store(file);
+            files.add(file.getOriginalFilename());
+
+            message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        } catch (Exception e) {
+            message = "FAIL to upload " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+        }
     }
-  }
- 
-  @GetMapping("/getallfiles")
-  public ResponseEntity<List<String>> getListFiles(Model model) {
-    List<String> fileNames = files
-        .stream().map(fileName -> MvcUriComponentsBuilder
-            .fromMethodName(UploadFileController.class, "getFile", fileName).build().toString())
-        .collect(Collectors.toList());
- 
-    return ResponseEntity.ok().body(fileNames);
-  }
- 
-  @GetMapping("/files/{filename:.+}")
-  @ResponseBody
-  public ResponseEntity<Resource> getFile(@PathVariable String filename) {
-    Resource file = storageService.loadFile(filename);
-    return ResponseEntity.ok()
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
-        .body(file);
-  }
+
+    @GetMapping("/getallfiles")
+    public ResponseEntity<List<String>> getListFiles(Model model) {
+        List<String> fileNames = files
+                .stream().map(fileName -> MvcUriComponentsBuilder
+                .fromMethodName(UploadFileController.class, "getFile", fileName).build().toString())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(fileNames);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.loadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
 }
